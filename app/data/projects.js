28b867496abc42d3a836bb58256c35c097b7bb2c
Benson-Craven <bensoncraven@hotmail.co.uk>
2024-11-20 16:45:11 +1100

    Added Nexaflow

--- app/data/projects.js
+++ app/data/projects.js
@@ -17,16 +17,16 @@ const projects = [
         id: 2,
         comingSoon: false,
     },
-    // {
-    //     url: "/images/nexaflow.avif",
-    //     gifUrl: "/images/nexaflow-placeholder.avif",
-    //     title: "Nexaflow",
-    //     link: "https://nexaflow.netlify.app",
-    //     id: 3,
-    //     comingSoon: false,
-    // },
     {
+        url: "/images/new-nexaflow-placeholder.png",
+        gifUrl: "/images/new-nexaflow.png",
+        title: "Nexaflow",
+        link: "https://nexaflow.netlify.app",
         id: 3,
+        comingSoon: false,
+    },
+    {
+        id: 4,
         title: "More to come",
         url: "", // Not needed for coming soon card
         gifUrl: "", // Not needed for coming soon card
